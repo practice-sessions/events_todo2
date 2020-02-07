846936746ff9a7b4d1c578ec869e453731d4ce49
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-07 15:31:04 +0100

    Create server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
