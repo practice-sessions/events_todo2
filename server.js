da77522c0342d421ae93f637c3afdfafc4477a99
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-07 22:18:09 +0100

    Connect to database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 6000;
+
+// Connect Database
+connectDB();
 
 app.use(cors());
 app.use(express.json({ extended: false }));
