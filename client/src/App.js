ff9d4af907ef0585688fac10127dbdb5f9cb9526
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-18 12:49:29 +0100

    Create todo components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Create_Todo from './components/create_todo';
+import Delete_Todo from './components/delete_todo';
+import Edit_Todo from './components/edit_todo';
+import Todos_List from './components/todos_list';
 
 class App extends Component {
   render() {
@@ -9,6 +13,10 @@ class App extends Component {
         <div className='container'>
           <h2>Pet Manager's Events Todo App</h2>
         </div>
+        <Route path='/' exact component={Todos_List} />
+        <Route path='/edit/:id' component={Edit_Todo} />
+        <Route path='/create' component={Create_Todo} />
+        <Route path='/delete/:id' component={Delete_Todo} />
       </Router>
     );
   }
