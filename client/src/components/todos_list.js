7e9b2c7a41d57b01034e2348e07c26e11cd33a05
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-18 12:57:23 +0100

    Modify todos list file

---
+++ client/src/components/todos_list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos_List extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is my Todos List Component !!</p>
+      </div>
+    );
+  }
+}
+
+export default Todos_List;
