352b2c9e24ee721d47eac4dc463ca1051fbd0e8d
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-18 15:10:42 +0100

    Add state properties to Create todo

--- client/src/components/create_todo.js
+++ client/src/components/create_todo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class Create_Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_startdate: '',
+      todo_completeddate: '',
+      todo_completed: false
+    };
+  }
+
   render() {
     return (
       <div>
