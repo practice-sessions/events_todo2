5271335478224ca36d4bf9f9ae16344194be42c2
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-18 13:14:04 +0100

    Modify Delete Todo file

---
+++ client/src/components/delete_todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Delete_Todo extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is Delete Todo Component</p>
+      </div>
+    );
+  }
+}
+
+export default Delete_Todo;
