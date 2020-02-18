2942c73dae3819385e2791c6ee5d30fcfdbf6768
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-18 12:37:52 +0100

    Add and inplement react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <h2>Pet Manager's Events Todo App</h2>
-      </div>
+      <Router>
+        <div className='container'>
+          <h2>Pet Manager's Events Todo App</h2>
+        </div>
+      </Router>
     );
   }
 }
