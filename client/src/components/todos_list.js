6b149cd174289497833c8056b4e998be0bb1587d
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-19 15:16:03 +0100

    Use componentDidmount hooks to get todos

--- client/src/components/todos_list.js
+++ client/src/components/todos_list.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+  <tr>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_description}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_responsible}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_priority}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_completeddate}
+    </td>
+    <td>
+      <Link to={'/edit/' + props.todo._id}>Edit</Link>
+    </td>
+    <td>
+      <Link to={'/delete/' + props.todo._id}>Delete</Link>
+    </td>
+  </tr>
+);
 
 class Todos_List extends Component {
   constructor(props) {
     super(props);
     this.state = { todos: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:6000/api/todos')
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
