3b74d3e6e1cab3128cd172263e2cfcb0fdb045ef
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-24 13:50:51 +0100

    Implement event handler for edit todo

--- client/src/components/edit_todo.js
+++ client/src/components/edit_todo.js
@@ -10,21 +10,72 @@ class Edit_Todo extends Component {
       todo_responsible: '',
       todo_priority: '',
       todo_startdate: '',
-      todo_completedate: '',
+      todo_completeddate: '',
       todo_completed: false
     };
   }
+
+  onChangeTodoDescription = e => {
+    this.setState({
+      todo_description: e.target.value
+    });
+  };
+
+  onChangeTodoResponsible = e => {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  };
+
+  onChangeTodoPriority = e => {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  };
+
+  onChangeTodoCompleted = e => {
+    this.setState({
+      todo_completed: !this.state.todo_completed
+    });
+  };
+
+  onChangeTodoStartDate = e => {
+    this.setState({
+      todo_startdate: e.target.value
+    });
+  };
+
+  onChangeTodoCompletedDate = e => {
+    this.setState({
+      todo_completeddate: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const obj = {
+      todo_description: this.state.todo_description,
+      todo_responsible: this.state.todo_responsible,
+      todo_priority: this.state.todo_priority,
+      todo_completed: this.state.todo_completed,
+      todo_startdate: this.state.todo_startdate,
+      todo_completeddate: this.state.todo_completeddate
+    };
+    console.log(obj);
+  };
+
   render() {
     return (
       <div>
         <h3 align='center'>Edit Todo</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className='form-group'>
             <label>Description:</label>
             <input
               type='text'
               className='form-control'
               value={this.state.todo_description}
+              onChange={this.onChangeTodoDescription}
             />
           </div>
 
@@ -34,6 +85,7 @@ class Edit_Todo extends Component {
               type='text'
               className='form-control'
               value={this.state.todo_responsible}
+              onChange={this.onChangeTodoResponsible}
             />
           </div>
 
@@ -43,6 +95,7 @@ class Edit_Todo extends Component {
               type='date'
               className='form-control'
               value={this.state.todo_startdate}
+              onChange={this.onChangeTodoStartDate}
             />
           </div>
 
@@ -52,6 +105,7 @@ class Edit_Todo extends Component {
               type='date'
               className='form-control'
               value={this.state.todo_completedate}
+              onChange={this.onChangeTodoCompleteDate}
             />
           </div>
 
@@ -64,6 +118,7 @@ class Edit_Todo extends Component {
                 id='priorityLow'
                 value='Low'
                 checked={this.state.todo_priority === 'Low'}
+                onChange={this.onChangeTodoPriority}
               />
               <label className='form-check-label'>Low</label>
             </div>
@@ -76,6 +131,7 @@ class Edit_Todo extends Component {
                 id='priorityMedium'
                 value='Medium'
                 checked={this.state.todo_priority === 'Medium'}
+                onChange={this.onChangeTodoPriority}
               />
               <label className='form-check-label'>Medium</label>
             </div>
@@ -87,6 +143,7 @@ class Edit_Todo extends Component {
                 id='priorityHigh'
                 value='High'
                 checked={this.state.todo_priority === 'High'}
+                onChange={this.onChangeTodoPriority}
               />
               <label className='form-check-label'>High</label>
             </div>
@@ -98,7 +155,9 @@ class Edit_Todo extends Component {
               id='completedCheckbox'
               type='checkbox'
               name='completedCheckbox'
+              onChange={this.onChangeTodoCompleted}
               checked={this.state.todo_completed}
+              value={this.state.todo_completed}
             />
             <label className='form-check-label' htmlFor='completedCheckbox'>
               Completed
