d9012eeae80fdfaded337cd8d78f089aad5431d5
Mekarosi <mekarosi_assa@yahoo.com>
2020-02-27 11:41:29 +0100

    Add form fields to delete todo

--- client/src/components/delete_todo.js
+++ client/src/components/delete_todo.js
@@ -15,7 +15,73 @@ class Delete_Todo extends Component {
   render() {
     return (
       <div>
-        <p>This is Delete Todo Component</p>
+        <h3 align='center'>Delete Todo</h3>
+        <form>
+          <div className='form-group'>
+            <label>Description: </label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.todo_description}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Responsible: </label>
+            <input
+              type='text'
+              className='form-control'
+              value={this.state.todo_responsible}
+            />
+          </div>
+
+          <div className='form-group'>
+            <div className='form-check-input'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='priorityLow'
+                value='Low'
+                checked={this.state.todo_priority === 'Low'}
+              />
+              <label className='form-check-label'>Low</label>
+            </div>
+
+            <div className='form-check form-check-inline'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='priorityMedium'
+                value='Medium'
+                checked={this.state.todo_priority === 'Medium'}
+              />
+              <label className='form-check-label'>Medium</label>
+            </div>
+
+            <div className='form-check form-check-inline'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='priorityOptions'
+                id='priorityHigh'
+                value='High'
+                checked={this.state.todo_priority === 'High'}
+              />
+              <label className='form-check-label'>High</label>
+            </div>
+          </div>
+          <br />
+
+          <div className='form-group'>
+            <input
+              type='submit'
+              value='Delete Todo'
+              className='btn btn-danger'
+            />
+          </div>
+        </form>
       </div>
     );
   }
