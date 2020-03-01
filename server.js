af46da56b289c9e7912b62e38887b8fe446253c2
Mekarosi <mekarosi_assa@yahoo.com>
2020-03-01 21:26:28 +0100

    Install and implement cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 // Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 // Define Route
 app.use('/api/todos', require('./route/api/todos'));
